2c13f8e1c602e2165bb477cd68939442603bcf07
shibadev <juliobragadelima@gmail.com>
2026-08-02 16:25:40 -0300

    Add Animal class with constructor and method

---
+++ retomada/Animal.java
@@ -0,0 +1,9 @@
+class Animal {
+    String nome;
+    public Animal(String nome) {
+        this.nome = nome;
+    }
+    public void fazerSom() {
+        System.out.println(nome + " fez um som.");
+    }
+}
