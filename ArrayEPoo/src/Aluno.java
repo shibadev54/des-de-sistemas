7c27c21a45d9700d7927c5986488b5c285f10b16
shibadev <juliobragadelima@gmail.com>
2026-09-02 16:14:20 -0300

    Atividade de arraylist usando poo

---
+++ ArrayEPoo/src/Aluno.java
@@ -0,0 +1,35 @@
+public class Aluno {
+    private String nome;
+    private int nota1;
+    private int nota2;
+
+    public Aluno(String nome, int nota1, int nota2) {
+        this.nome = nome;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(int nota1) {
+        this.nota1 = nota1;
+    }
+
+    public int getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(int nota2) {
+        this.nota2 = nota2;
+    }
+}
